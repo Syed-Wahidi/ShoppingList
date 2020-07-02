5e8a1b88535b612d812401a67fbdf0eb4e819f95
Syed-Wahidi <47909303+Syed-Wahidi@users.noreply.github.com>
2020-07-01 18:08:29 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+$( ".shopping-list" ).on("click",".shopping-item-toggle", function() {
+ $(event.target).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
+ 
+});
+
+
+
+  $('#js-shopping-list-form' ).on("submit", function(event) {
+    event.preventDefault();
+    var listItem = $('#shopping-list-entry').val();
+
+    $('#shopping-list-entry').val('');
+    $('.shopping-list').append(
+            `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+
+	$( ".shopping-list" ).on("click",".shopping-item-delete", function() {
+$(event.target).closest("li").remove(  );
+});
+  
+
+
